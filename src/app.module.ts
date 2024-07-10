4ea3e6bfe416cf33826192760c8a75d540f479f5
Rymjbeli <rim.jbeli@insat.ucar.tn>
2024-07-10 18:05:32 +0100

    Database and graphql setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,40 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelmetMiddleware } from '@nest-middlewares/helmet';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      debug: false,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule{
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(HelmetMiddleware).forRoutes('');
   }
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,11 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'node:path';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: [
-      process.env.FRONTEND_PORT_1,
-      process.env.FRONTEND_PORT_2,
-    ],
+    origin: [process.env.FRONTEND_PORT_1, process.env.FRONTEND_PORT_2],
     credentials: true,
   });
   app.useStaticAssets(path.join(__dirname, '../uploads'));
